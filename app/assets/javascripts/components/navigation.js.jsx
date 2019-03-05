2dc168593bef7f0d0b163106f77690f77e8a7d0d
crismanz <crismanz@github.com>
2019-03-05 05:05:57 +0000

    added some style

--- app/assets/javascripts/components/navigation.js.jsx
+++ app/assets/javascripts/components/navigation.js.jsx
@@ -2,31 +2,25 @@ class Navigation extends React.Component {
 
     render() {
       return (
-            <div className="navbar navbar-expand-lg navbar-light bg-light">
-                    <a className="navbar-brand" href="/"><h6>askHelp</h6></a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                      </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item">
-                          <a className="nav-link" href="/site/about"><h6>About</h6></a>
-                        </li>
-                        <li className="nav-item">
-                          <a className="nav-link" href="/requests"><h6>Requests</h6></a>
-                        </li>
-                        <li className="nav-item">
-                          <a className="nav-link" href="/requests/new"><h6>Add a new request</h6></a>
-                        </li>
-                        <li className="nav-item">
-                          <a className="nav-link" href="/conversations"><h6>Messages</h6></a>
-                        </li>
-                        <li className="nav-item">
-                          <a className="nav-link" href="/site/contact"><h6>Contact</h6></a>
-                        </li>
-                    </ul>
-                    </div>
-            </div>
+            <div className="navbar navbar-default navbar-static-top" role= "navgiation">
+              <div className="container">
+                <div className="navbar-header">
+                  <button className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span className="icon icon-bar"></span>
+                    <span className="icon icon-bar"></span>
+                    <span className="icon icon-bar"></span>
+                  </button>
+                  <a href="/" className="navbar-brand">askHelp</a>
+                </div>
+                <div className="collapse navbar-collapse">
+               <ul className="nav navbar-nav navbar-right">
+                    <li><a href="/requests">Requests</a></li>
+                    <li><a href="/requests/new">Add a new request</a></li>
+                    <li><a href="/conversations">Messages</a></li>
+               </ul>
+          </div>
+          </div>
+          </div>
       );
     }
   };
